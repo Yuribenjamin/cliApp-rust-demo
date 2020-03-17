1d25c1d6f75dc44f4e73a1c1383452b546655616
Ibrahim Ragab <Abrahammoustafa@hotmail.co.uk>
2020-03-17 13:46:14 +0200

    ownership ; borrowing

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 #![allow(non_snake_case)]
 fn main() {
     let first = "Ibrahim".to_string();
-    let last = "Ragab".to_string();
-
-    say_name(first, last)
+    say_name(&first);
+    say_name(&first);
 }
 
-fn say_name(first: String, last: String) {
-    println!("{} {}", first, last)
+fn say_name(first: &String) {
+    println!("{}", first)
 }
+
+/*function has been called with this value, we can no longer use this variable
+ pass it by reference instead of by value using the & symbol and by updating the function's signature
+*/
